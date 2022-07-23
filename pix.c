0edb77b440d7c1be75695dd99309228e6833fd94
sdk250 <520sdk250@gmail.com>
2022-07-23 13:59:34 +0800

    update

--- pix.c
+++ pix.c
@@ -10,7 +10,8 @@
 #include "cJSON.h"
 #define ERRMSG "\33[1;31mError: \33[0m"
 #define PORT 80
-#define BUFSIZE 2000
+#define BUFSIZE 2048
+#define SOCKBUF 2097152
 #define API_H5 "h5.pipix.com"
 #define API_PIX "is.snssdk.com"
 #define USER_AGENT "Mozilla/5.0 (Linux; Android 12; XT2201-2) " \
@@ -18,7 +19,7 @@
 #define ACCEPT "Application/json; Charset: UTF-8"
 #define CONNECTION "Close"
 
-char *fn(char *restrict, const char *restrict, const char *restrict);
+unsigned long int fn(char *restrict, const char *restrict, const char *restrict);
 char *regex(char *restrict, const char *restrict);
 
 int main(int argc, char **argv)
@@ -30,9 +31,10 @@ int main(int argc, char **argv)
 	}
 	char *msg = (char *)malloc(BUFSIZE * sizeof(char));
 	char *addr = (char *)malloc(BUFSIZE * sizeof(char));
-	char *buffer = (char *)malloc(BUFSIZE * 50 * sizeof(char));
-	char *buffer_ = (char *)malloc(BUFSIZE * 50 * sizeof(char));
+	char *buffer = (char *)malloc(SOCKBUF * sizeof(char));
+	char *buffer_ = (char *)malloc(SOCKBUF * sizeof(char));
 	char *god_comment = NULL;
+	char *ch = NULL;
 	cJSON *json = NULL;
 	cJSON *obj = NULL;
 	cJSON *o = NULL;
@@ -40,7 +42,8 @@ int main(int argc, char **argv)
 
 	memset(addr, '\0', BUFSIZE * sizeof(char));
 	memset(msg, '\0', BUFSIZE * sizeof(char));
-	memset(buffer, '\0', BUFSIZE * 50 * sizeof(char));
+	memset(buffer, '\0', SOCKBUF * sizeof(char));
+	memset(buffer_, '\0', SOCKBUF * sizeof(char));
 	strcpy(addr, regex(argv[1], "/{1}[a-z0-9A-Z]{5,}") + 1);
 	sprintf(msg, "GET /s/%s HTTP/1.1\n"
 		"Host: %s\n"
@@ -51,12 +54,12 @@ int main(int argc, char **argv)
 		"{}",
 		addr, API_H5, USER_AGENT, ACCEPT, CONNECTION
 	);
-	buffer = fn(buffer, msg, API_H5);
+	fn(buffer, msg, API_H5);
 
 	memset(msg, '\0', BUFSIZE * sizeof(char));
 	memset(addr, '\0', BUFSIZE * sizeof(char));
 	strcpy(addr, regex(buffer, "item/?[0-9]{10,}"/*"(http|https)://h5.?pipix.?com/{1}item/{1}[0-9]{1,}\\?"*/) + 5);
-	memset(buffer, '\0', BUFSIZE * 50 * sizeof(char));
+	memset(buffer, '\0', SOCKBUF * sizeof(char));
 	sprintf(msg, "GET /bds/cell/detail/?cell_type=1&aid=1319&app_name=super&cell_id=%s HTTP/1.1\n"
 		"Host: %s\n"
 		"User-Agent: %s\n"
@@ -66,11 +69,23 @@ int main(int argc, char **argv)
 		"{}",
 		addr, API_PIX, USER_AGENT, ACCEPT, CONNECTION
 	);
-	buffer = fn(buffer, msg, API_PIX);
+	fn(buffer, msg, API_PIX);
 
 	memset(msg, '\0', BUFSIZE * sizeof(char));
 	memset(addr, '\0', BUFSIZE * sizeof(char));
 	strcpy(buffer_, strchr(buffer, '{'));
+	ch = strchr(buffer_, '\n');
+	*ch = '\0';
+	strcat(buffer_, ch + 5);
+	/*unsigned int j = 0;
+	for (unsigned int i = 0; *(buffer_ + i) != '\0'; i++)
+	{
+		if (*(buffer + i) == '\n'
+	*/
+	printf("%s\nlength: %zd\nstrlen: %zd\n", buffer_, sizeof(buffer_), strlen(buffer_));
+	
+	fp = fopen("data", "w");
+	fwrite(buffer_, sizeof(char), strlen(buffer_), fp);
 	json = cJSON_Parse(buffer_);
 	obj = cJSON_GetObjectItem(cJSON_GetObjectItem(json, "data")->child, "item");
 	o = cJSON_GetObjectItem(obj, "comments");
@@ -78,7 +93,7 @@ int main(int argc, char **argv)
 	printf("%d\n", o->type);
 	puts(god_comment);
 
-	// fclose(fp);
+	fclose(fp);
 	cJSON_Delete(json);
 	free(msg);
 	free(addr);
@@ -87,11 +102,11 @@ int main(int argc, char **argv)
 	return 0;
 }
 
-char *fn(char *restrict buffer, const char *restrict msg, const char *restrict addr)
+unsigned long int fn(char *restrict buffer, const char *restrict msg, const char *restrict addr)
 {
 	int sockfd = -1;
 	struct sockaddr_in addr_;
-	struct hostent *host;
+	struct hostent *host = NULL;
 	long int recvb = 0;
 	unsigned long int recvbyte = 0;
 
@@ -109,32 +124,31 @@ char *fn(char *restrict buffer, const char *restrict msg, const char *restrict a
 	if ((connect(sockfd, (struct sockaddr *)&addr_, sizeof(struct sockaddr))) < 0)
 		fprintf(stderr, "%sConnect to %s of %s fail.\n", ERRMSG, inet_ntoa(addr_.sin_addr), addr);
 	printf("Connect to \33[1;46m%s\33[0m of %s success.\n", inet_ntoa(addr_.sin_addr), addr);
-	if ((write(sockfd, msg, strlen(msg))) < 0)
+	if ((send(sockfd, msg, strlen(msg), MSG_WAITALL)) < 0)
 		fprintf(stderr, "%sRequest %s fail.\n", ERRMSG, addr);
-	while ((recvb = read(sockfd, buffer + recvbyte, BUFSIZE * 50 - recvbyte)) > 0)
+	while ((recvb = recv(sockfd, buffer + recvbyte, SOCKBUF, MSG_WAITALL)) > 0)
 		recvbyte += recvb;
 	close(sockfd);
-	return buffer;
+	return recvbyte;
 }
 
 char *regex(char *restrict string, const char *restrict pattern)
 {
-	char *buffer = (char *)malloc(BUFSIZE * 10 * sizeof(char));
+	char *buffer = (char *)malloc(SOCKBUF * sizeof(char));
 	char ebuf[256] = {'\0'};
 	regex_t reg;
 	const size_t nmatch = 10;
 	regmatch_t pmatch[nmatch];
 	int reg_err_code = 0;
 
-	memset(buffer, '\0', BUFSIZE * 10 * sizeof(char));
+	memset(buffer, '\0', SOCKBUF * sizeof(char));
 	if ((reg_err_code = regcomp(&reg, pattern, REG_EXTENDED)) != 0)
 	{
 		regerror(reg_err_code, &reg, ebuf, sizeof(ebuf) / sizeof(char));
 		fprintf(stderr, "%s%s: Pattern: %s\n", ERRMSG, ebuf, pattern);
 		exit(EXIT_FAILURE);
 	}
-	reg_err_code = regexec(&reg, string, nmatch, pmatch, 0);
-	if (reg_err_code == REG_NOMATCH)
+	if ((reg_err_code = regexec(&reg, string, nmatch, pmatch, 0)) == REG_NOMATCH)
 	{
 		printf("%s\n%s\n", "Not found result: Your share link is error.", string);
 		exit(EXIT_SUCCESS);
